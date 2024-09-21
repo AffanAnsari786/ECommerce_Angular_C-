a5aaef868d784319207cc1acbdb31239ae52aa76
Affan Ansari <aansari2@zarca.com>
2024-09-22 00:45:29 +0530

    Remove environments folder from tracking

--- FrontendPart/src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    feedPath: 'https://localhost:7011/api/Posts/lists',
-    userPath: 'https://localhost:7011/api/Users',
-    basepostpath : 'https://localhost:7011/api/Posts',
-    baseproducts : 'https://fakestoreapi.com'
-};
